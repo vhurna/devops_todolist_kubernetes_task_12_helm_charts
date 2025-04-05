5aee5253133236ba8f0eec07cdccae2d6821bcae
mira <mkyshchak@gmail.com>
2025-04-05 18:56:03 +0300

    k8s12

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
+# Створіть кластер
+kind create cluster --config cluster.yml
+
+# Забрудніть вузли
+kubectl taint node <node_name> app=mysql:NoSchedule
+
+# Розгорніть ресурси для бази даних
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
 kubectl apply -f .infrastructure/mysql/service.yml
 kubectl apply -f .infrastructure/mysql/statefulSet.yml
 
+# Розгорніть ресурси для застосунку
 kubectl apply -f .infrastructure/app/ns.yml
 kubectl apply -f .infrastructure/app/pv.yml
 kubectl apply -f .infrastructure/app/pvc.yml
@@ -15,6 +24,9 @@ kubectl apply -f .infrastructure/app/nodeport.yml
 kubectl apply -f .infrastructure/app/hpa.yml
 kubectl apply -f .infrastructure/app/deployment.yml
 
-# Install Ingress Controller
+# Розгорніть Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
+
+# Розгорніть helm-чарт (необов'язково, якщо ви хочете використовувати тільки існуючі ресурси)
+# helm upgrade --install todoapp helm-chart/todoapp
